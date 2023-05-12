044cce6ef2cab5ce56e97cf285fe7f22441aa3f6
itbebop <itbebop@gmail.com>
2023-05-12 14:37:30 +0900

    Mockito를 활용하여 pixabay_api 테스트

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -5,11 +5,13 @@ import '../model/Photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi implements PhotoApiRepository{
-  final baseUrl = 'https://pixabay.com/api/';
-  final key = '36206009-fe028570b3c99a317c543088c';
+  static const baseUrl = 'https://pixabay.com/api/';
+  static const key = '36206009-fe028570b3c99a317c543088c';
 
   @override
-  Future<List<Photo>> fetch(String querry) async {
+  Future<List<Photo>> fetch(String querry, {http.Client? client}) async {
+    client ??= http.Client(); // client가 null일 떄는 초기화를 하겠다는 것
+
     final response = await http.get(Uri.parse(
         '$baseUrl?key=$key&q=$querry&image_type=photo'));
 
